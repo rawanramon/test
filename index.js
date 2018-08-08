de1c4cfb0db99c34118cc7923d920cb36fcfa9c5
Erwan REMOND <erwan_remond@yahoo.fr>
2018-08-08 11:38:42 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,9 +9,16 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', function(req, res) {if ((!req.query.maxAge) || (req.query.maxAge> 360)) {occurence=300/30*170000;} else {occurence=req.query.maxAge/30*170000;}
+  .get('/', function(req, res) {if ((!req.query.maxAge) || (req.query.maxAge> 300)) {occurence=300/30*17000;} else {occurence=req.query.maxAge/30*17000;}
+  result.LOCATION=[];
   for (var i = 0; i < occurence; i++) {
 	result.LOCATION.push(pos);
+	if (i == 5000) {
+		result.LOCATION.push(["3156490059059000000",6.145409,51.381982,"2018-08-09T05:33:15Z",44,27546,"FR",4,3800]);
 	}
+	if (i == 15000) {
+		result.LOCATION.push(["3156490059059000000",6.145409,51.381982,"2017-08-09T04:33:15Z",44,27546,"FR",4,3800]);
+	}
+  }
   res.send(result)})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
