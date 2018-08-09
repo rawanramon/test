c65dff199eeb678ee6e3de1677ddb590620caf9f
Erwan REMOND <erwan_remond@yahoo.fr>
2018-08-09 10:08:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,40 +9,60 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', function(req, res) {if ((!req.query.maxAge) || (req.query.maxAge> 300)) {occurence=300;} else {occurence=req.query.maxAge;}
-  result.LOCATION=[];
-  for (var i = 0; i < occurence * 1700000 / (300 * (1+Math.random())); i++) {
-	dt=new Date();
-	dt.setSeconds(dt.getSeconds()-35);
-	if (dt.getMonth()<10) {
-		dateTxt=dt.getFullYear()+'-0'+dt.getMonth()
-	} else {
-		dateTxt=dt.getFullYear()+'-'+dt.getMonth()
+  .get('/', function(req, res) {
+	if ((!req.query.maxAge) || (req.query.maxAge> 300)) 
+		{occurence=300;} 
+	else 
+		{occurence=req.query.maxAge;}
+
+	var acceptEncoding = req.headers['accept-encoding'];
+	if (!acceptEncoding) {
+		acceptEncoding = '';
 	}
-	if (dt.getDate()<10) {
-		dateTxt+='-0'+dt.getDate()
-	} else {
-		dateTxt+='-'+dt.getDate()
-	}
-	if (dt.getHours()<10) {
-		dateTxt+='T0'+dt.getHours()
-	} else {
-		dateTxt+='T'+dt.getHours()
-	}
-	if (dt.getMinutes()<10) {
-		dateTxt+=':0'+dt.getMinutes()
-	} else {
-		dateTxt+=':'+dt.getMinutes()
-	}
-	if (dt.getSeconds()<10) {
-		dateTxt+=':0'+dt.getSeconds()
-	} else {
-		dateTxt+=':'+dt.getSeconds()
-	}
-	dateTxt+='Z'						
 	
-	result.LOCATION.push(["3156490059059000000",6.145409,51.381982,dateTxt,44,27546,"FR",4,3800]);
-	
-  }
-  res.send(result)})
+	  result.LOCATION=[];
+	  for (var i = 0; i < occurence * 1700000 / (300 * (1+Math.random())); i++) {
+		dt=new Date();
+		dt.setMonth(8);
+		dt.setMinutes(dt.getMinutes()+119);
+		if (dt.getMonth()<10) {
+			dateTxt=dt.getFullYear()+'-0'+dt.getMonth()
+		} else {
+			dateTxt=dt.getFullYear()+'-'+dt.getMonth()
+		}
+		if (dt.getDate()<10) {
+			dateTxt+='-0'+dt.getDate()
+		} else {
+			dateTxt+='-'+dt.getDate()
+		}
+		if (dt.getHours()<10) {
+			dateTxt+='T0'+dt.getHours()
+		} else {
+			dateTxt+='T'+dt.getHours()
+		}
+		if (dt.getMinutes()<10) {
+			dateTxt+=':0'+dt.getMinutes()
+		} else {
+			dateTxt+=':'+dt.getMinutes()
+		}
+		if (dt.getSeconds()<10) {
+			dateTxt+=':0'+dt.getSeconds()
+		} else {
+			dateTxt+=':'+dt.getSeconds()
+		}
+		dateTxt+='Z'						
+		
+		result.LOCATION.push(["3156490059059000000",6.145409,51.381982,dateTxt,44,27546,"FR",4,3800]);
+		
+	  }
+	if (acceptEncoding = 'gzip') {
+		res.writeHead(200, {'Content-Encoding': 'gzip'});
+		zlib.gzip(result, function (_, resultzip) {  // The callback will give you the 
+			res.end(resultzip);                     // result, so just send it.
+		});
+	} else {
+		res.send(result);	
+	}
+		
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
